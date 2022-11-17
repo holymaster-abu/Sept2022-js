e4a67712812ecadb1916f9cadf7e679941e6bb9f
Yarema Kulchytskyi <kulchytskyi.yarema@gmail.com>
2022-11-17 16:53:30 +0200

    fix

---
+++ Homeworks/JS/hw4/main.js
@@ -0,0 +1,116 @@
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+
+let x = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, nesciunt."
+for (let i = 0; i < 10; i++) {
+    document.write(`<div>${x}</div>`)
+
+}
+
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+
+for (let i = 0; i < 10; i++) {
+    document.write(`<div>${i} ${x}</div>`)
+
+}
+
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+
+let y = "Lorem ipsum dolor sit amet."
+let i = 0;
+while (i < 20) {
+    i++
+    document.write(`<h1>${y}</h1>`)
+}
+
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+
+let a = "Lorem ipsum dolor sit amet."
+let ia = 0;
+while (ia < 20) {
+    ia++
+    document.write(`<h1>${a} ${ia}</h1>`)
+}
+
+// - Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону
+
+let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'angular', 'node.js'];
+document.write(`<ul>`);
+for ( let item of listOfItems) {
+    document.write (`<li>${item}</li>`)
+    }
+
+    document.write (`</ul>`);
+
+// Використовуючи данні з масиву, за допомоги document.write та циклу
+// побудувати структуру по шаблону  Зробити адекватну стилізацію
+// Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
+
+let products = [
+    {
+        title: 'milk',
+        price: 22,
+        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+    },
+    {
+        title: 'juice',
+        price: 27,
+        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+    },
+    {
+        title: 'tomato',
+        price: 47,
+        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+    },
+    {
+        title: 'tea',
+        price: 15,
+        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+    },
+];
+
+for (let product of products) {
+    document.write(`
+<div product-card >
+    <h3 class=product-title>Title -   ${product.title} Price - ${product.price}</h3>
+    <img src="${product.image}" alt="${product.title}" class=product-image>
+ </div>`)
+}
+
+let users = [
+    {name: 'vasya', age: 31, status: false},
+    {name: 'petya', age: 30, status: true},
+    {name: 'kolya', age: 29, status: true},
+    {name: 'olya', age: 28, status: false},
+    {name: 'max', age: 30, status: true},
+    {name: 'anya', age: 31, status: false},
+    {name: 'oleg', age: 28, status: false},
+    {name: 'andrey', age: 29, status: true},
+    {name: 'masha', age: 30, status: true},
+    {name: 'olya', age: 31, status: false},
+    {name: 'max', age: 31, status: true}
+];
+
+// за допомоги циклу вивести:
+//     - користувачів зі статусом true
+
+
+for (let user of users) {
+    if (user.status)
+        document.write(`<div>${user.name}</div>`)
+}
+
+// - користувачів зі статусом false
+
+for (let user of users) {
+    if (!user.status)
+        document.write(`<div>${user.name}</div>`)
+}
+
+// - користувачів які старші за 30 років
+
+for (let user of users) {
+    if (user.age > 30)
+        document.write(`<div>${user.name}</div>`)
+}
+
